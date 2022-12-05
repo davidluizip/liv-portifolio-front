0b7222a1b3c3ddee8bd4bd25ea5e010d2138d8f4
Yago Neno <developer.neno@gmail.com>
2022-12-05 10:34:50 -0300

    feat: pagina de inputs de registros

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -8,10 +8,25 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CoverFrontBacksideComponent } from './components/cover-front-backside/cover-front-backside.component';
 import { CoverFrontComponent } from './components/cover-front/cover-front.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterSelectModalComponent } from './pages/register/components/register-select-modal/register-select-modal.component';
+import { StudentSpeechRecordModalComponent } from './pages/register/components/student-speech-record-modal/student-speech-record-modal.component';
+import { RegisterBaseHeaderModalComponent } from './pages/register/components/register-base-header-modal/register-base-header-modal.component';
 
 @NgModule({
-  declarations: [BookComponent, CoverFrontBacksideComponent, CoverFrontComponent, IntroductionComponent],
+  declarations: [
+    BookComponent,
+    CoverFrontBacksideComponent,
+    CoverFrontComponent,
+    IntroductionComponent,
+    RegisterComponent,
+    RegisterSelectModalComponent,
+    StudentSpeechRecordModalComponent,
+    RegisterBaseHeaderModalComponent,
+  ],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
     BookRoutingModule,
     SharedModule,
