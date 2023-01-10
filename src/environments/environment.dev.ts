65bca852cecda0904eff4fc6585c7861b8cc68b6
Yago Neno <developer.neno@gmail.com>
2023-01-10 16:02:49 -0300

    chore: development environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  cypher_secret: '3595670729e25c22710796962d3cc7c8',
+  // baseUserStrapi: 'https://strapi-ci-e4be4tejsa-uc.a.run.app/api/',
+  baseUserStrapi: 'http://localhost:4000/api',
+  tokenStrapi:
+    'd0501b70c8285e8fb0c8a4197d02634bec8146b88efbff38303ae066d4a2085d5d130c179955f8a5b5a765e44f7ee2f55dc50da74232ea49c29d374a31555f7a4cb251e2243884e3ed076bac3ce0c7e426cd17b07f7d8e22345c9a7d584c2d5412a90b10e230c7d931e7cb83fcf7bcc478a2ef02b00532279cef4dadea705074',
+};
