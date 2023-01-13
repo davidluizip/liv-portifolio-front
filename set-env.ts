07e69d79161bc33d064d636acf9e17c672de7171
David Oliveira <davidluizip@gmail.com>
2023-01-13 19:26:26 -0300

    token

---
+++ set-env.ts
@@ -0,0 +1,31 @@
+import { writeFile } from 'fs';
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.ts';
+// Load node modules
+const colors = require('colors');
+require('dotenv').load();
+// `environment.ts` file structure
+const envConfigFile = `export const environment = {
+  tokenStrapi: '${process.env.TOKEN_PORTFOLIO}',
+  production: true,
+  cypher_secret: '3595670729e25c22710796962d3cc7c8',
+  baseUserStrapi: 'https://strapi-ci-e4be4tejsa-uc.a.run.app/api/',
+};
+`;
+console.log(
+  colors.magenta(
+    'The file `environment.ts` will be written with the following content: \n'
+  )
+);
+console.log(colors.grey(envConfigFile));
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      colors.magenta(
+        `Angular environment.ts file generated correctly at ${targetPath} \n`
+      )
+    );
+  }
+});
