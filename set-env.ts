458599a3c791d47126122175d23b942a4fb7065e
David Oliveira <davidluizip@gmail.com>
2023-01-13 19:35:35 -0300

    token

--- set-env.ts
+++ set-env.ts
@@ -6,7 +6,7 @@ const colors = require('colors');
 require('dotenv').load();
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
-  tokenStrapi: '${process.env.TOKEN_PORTFOLIO}',
+  tokenStrapi: '${process.env['TOKEN_PORTFOLIO']}',
   production: true,
   cypher_secret: '3595670729e25c22710796962d3cc7c8',
   baseUserStrapi: 'https://strapi-ci-e4be4tejsa-uc.a.run.app/api/',
