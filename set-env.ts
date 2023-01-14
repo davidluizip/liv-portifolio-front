5cf635a44de73d409b8e19f9b8b84506e2d6beda
David Oliveira <davidluizip@gmail.com>
2023-01-13 21:37:59 -0300

    token

--- set-env.ts
+++ set-env.ts
@@ -6,7 +6,7 @@ const colors = require('colors');
 require('dotenv').config();
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
-  tokenStrapi: '${process.env['_TOKEN_PORTFOLIO']}',
+  tokenStrapi: '659dcdcff121d9c1001cff46a0d15dc2f77d951a64c65637f998984091e432fa555673c965f51fe0439e446570498bfb497cd6fd6694032678e3731627da154ef88c5bd40594e88ca0785eb585fc9f7572179b39aee06771c67171868b37d625789fb106eaa963d3d3bc2faeda154b372a9a78590f096e8e46f9fcea84ad55be',
   production: true,
   cypher_secret: '3595670729e25c22710796962d3cc7c8',
   baseUserStrapi: 'https://strapi-ci-e4be4tejsa-uc.a.run.app/api/',
@@ -14,7 +14,7 @@ const envConfigFile = `export const environment = {
 `;
 console.log(
   colors.magenta(
-    'The file `environment.ts` will be written with the following content: \n'
+    'The file `environment.prod.ts` will be written with the following content: \n'
   )
 );
 console.log(colors.grey(envConfigFile));
