51eb1e881f79b17f21aeb7d499e85f9c660565e2
David Oliveira <davidluizip@gmail.com>
2023-01-26 21:03:22 -0300

    fix env

--- set-env.ts
+++ set-env.ts
@@ -10,7 +10,8 @@ const envConfigFile = `export const environment = {
   production: true,
   cypher_secret: '3595670729e25c22710796962d3cc7c8',
   baseUserStrapi: 'https://strapi-ci-e4be4tejsa-uc.a.run.app/api/',
-  apiLiv: "https://api.inteligenciadevida.com.br/api"
+  apiLiv: "https://api.inteligenciadevida.com.br/api",
+  stage: 'production'
 };
 `;
 console.log(
